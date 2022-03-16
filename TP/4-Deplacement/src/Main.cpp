e0ed8ebf5a412efe0e11fa1eab037166a3d0ae06
unknown <lucie@naffien.com>
2022-03-16 13:23:24 +0100

    TP note : deplacement

--- TP/4-Deplacement/src/Main.cpp
+++ TP/4-Deplacement/src/Main.cpp
@@ -5,77 +5,171 @@
 #include<iostream>
 
 using namespace cv;
+using namespace std;
 
-#define IMG_PATH "data/c_img1.png"
+#define IMG_FRONT_PATH "data/front.png"
+#define IMG_BACK_PATH "data/back.png"
+#define IMG_RESULT_PATH "data/result.png"
+
+#define IMG_BLUR_COEF 9
+#define BLUR_DEPTH 3
+
+#define DEPLACEMENT_COL -250
+#define DEPLACEMENT_ROW -30
+
+#define COLOR_NUMBER 3
+#define INDEX_BLUE 2
+#define INDEX_GREEN 1
+#define INDEX_RED 0
+
 
 int main( int argc, char** argv )
 {
-  Mat imageColor;
-  Mat imageBlur;
-  Mat imageBlurG;
-  Mat imageBlurM;
-  Mat imageFilterBilateral;
-  Mat imageFilterBox;
-  Mat imageSobel1;
-  Mat imageSobel2;
-  Mat imageSobel3;
-  Mat imageScharr1;
-  Mat imageScharr2;
-  Mat imageLaplacien;
-  Mat canny;
+	// Data initialization
+	Mat imgBack;
+	Mat imgFront;
 
-  imageColor = imread( IMG_PATH, 1 );
+	Mat imgBackBlur;
+	Mat imgFrontBlur;
 
-  if(!imageColor.data )
-    {
-	  std::cout << "No image data \n";
-      return -1;
+	Mat imgBackBinary;
+	Mat imgFrontBinary;
+
+	Mat imgSubtract;
+	Mat imgSubtractBinary;
+	Mat imgSubtractDilated;
+	Mat imgSubtractEroded;
+
+	vector<Mat> imgFrontColorSplit;
+	vector<Mat> imgResult;
+	Mat imgResultColor;
+
+	// Read input images
+	imgBack = imread( IMG_BACK_PATH, 1 );
+	imgFront = imread( IMG_FRONT_PATH, 1 );
+
+	if(!imgBack.data || !imgFront.data )
+	{
+		cout << "No image data \n";
+		return -1;
     }
 
-  blur(imageColor, imageBlur, Size(9, 9), Point(-1, -1), BORDER_DEFAULT);
-  GaussianBlur(imageColor, imageBlurG, Size(9, 9), 0, 0, BORDER_DEFAULT);
-  medianBlur(imageColor, imageBlurM, 9);
-  bilateralFilter(imageColor, imageFilterBilateral, 9, 75, 75, BORDER_DEFAULT);
-  boxFilter(imageColor, imageFilterBox, -1, Size(9, 9), Point(-1, -1), true, BORDER_DEFAULT);
-  Sobel(imageColor, imageSobel1, -1, 0, 1, 3, 1, 0, BORDER_DEFAULT);
-  Sobel(imageColor, imageSobel2, -1, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-  Sobel(imageColor, imageSobel3, -1, 1, 1, 3, 1, 0, BORDER_DEFAULT);
-  Scharr(imageColor, imageScharr1, -1, 1, 0, 1, 0, BORDER_DEFAULT);
-  Scharr(imageColor, imageScharr2, -1, 0, 1, 1, 0, BORDER_DEFAULT);
-  Laplacian(imageColor, imageLaplacien, -1, 1, 1, 0, BORDER_DEFAULT);
-  Canny(imageColor, canny, 100, 200, 3, false);
+	// Adapt the size of input images
+	Size size;
+	if(imgBack.size < imgFront.size)
+	{
+		size = imgBack.size();
+		resize(imgFront, imgFront, size);
+	}
+	else if(imgBack.size > imgFront.size)
+	{
+		size = imgFront.size();
+		resize(imgBack, imgBack, size);
+	}
 
-  namedWindow( "Initial Image", WINDOW_AUTOSIZE );
-  namedWindow( "Blur", WINDOW_AUTOSIZE );
-  namedWindow( "Gaussien Blur", WINDOW_AUTOSIZE );
-  namedWindow( "Median Blur", WINDOW_AUTOSIZE );
-  namedWindow( "Bilateral Filter", WINDOW_AUTOSIZE );
-  namedWindow( "Box Filter", WINDOW_AUTOSIZE );
-  namedWindow( "Sobel 1", WINDOW_AUTOSIZE );
-  namedWindow( "Sobel 2", WINDOW_AUTOSIZE );
-  namedWindow( "Sobel 3", WINDOW_AUTOSIZE );
-  namedWindow( "Scharr 1", WINDOW_AUTOSIZE );
-  namedWindow( "Scharr 2", WINDOW_AUTOSIZE );
-  namedWindow( "Laplacien", WINDOW_AUTOSIZE );
-  namedWindow( "Canny", WINDOW_AUTOSIZE );
+	// Initializes all other matrices
+	imgResultColor = imgBack.clone();
+	imgFrontBinary.create(size, CV_8U);
+	imgBackBinary.create(size, CV_8U);
+	imgResultColor.create(size, CV_8U);
 
+	// Gaussian blur to reduce noise due to the size reduction
+	GaussianBlur(imgFront, imgFrontBlur, Size(IMG_BLUR_COEF, IMG_BLUR_COEF), 18, 0, 4);
+	GaussianBlur(imgBack, imgBackBlur, Size(IMG_BLUR_COEF, IMG_BLUR_COEF), 18, 0, 4);
 
+	// Transformation into gray images
+	cvtColor(imgBackBlur, imgBackBinary, COLOR_BGR2GRAY);
+	cvtColor(imgFrontBlur, imgFrontBinary, COLOR_BGR2GRAY);
 
-  imshow( "Initial Image", imageColor );
-  imshow( "Blur", imageBlur );
-  imshow( "Gaussien Blur", imageBlurG );
-  imshow( "Median Blur", imageBlurM );
-  imshow( "Bilateral Filter", imageFilterBilateral );
-  imshow( "Box Filter", imageFilterBox );
-  imshow( "Sobel 1", imageSobel1 );
-  imshow( "Sobel 2", imageSobel2 );
-  imshow( "Sobel 3", imageSobel3 );
-  imshow( "Scharr 1", imageScharr1 );
-  imshow( "Scharr 2", imageScharr2 );
-  imshow( "Laplacien", imageLaplacien );
-  imshow( "Canny", canny );
+	// Subtraction of the 2 binary images to detect their differences
+	subtract(imgBackBinary, imgFrontBinary, imgSubtract);
 
-  waitKey(0);
+	// Noise reduction with Gaussian Blur
+	for(int j = 0; j < BLUR_DEPTH; j++)
+	{
+		GaussianBlur(imgSubtract, imgSubtract, Size(3, 3), 15, 0, 4);
+	}
+
+	/*
+	 * Added after the teacher's correction
+	 */
+
+	// Binarization
+	threshold(imgSubtract, imgSubtractBinary, 50, 255, THRESH_OTSU + THRESH_BINARY);
+
+	// Dilation to fill the selected object
+	dilate(imgSubtractBinary, imgSubtractDilated, Mat(), Point(-1, -1), 18);
+
+	// Erosion
+	erode(imgSubtractDilated, imgSubtractEroded, Mat(), Point(-1, -1), 16);
+
+	/*
+	 * End of the added part after the teacher's correction
+	 */
+
+	// Split images color
+	split(imgFront, imgFrontColorSplit);
+	split(imgBack, imgResult);
+
+	// Draw selected object in the back image, with a different position
+	for (int x = 0; x < imgResultColor.rows; x++)
+	{
+		for (int y = 0; y < imgResultColor.cols; y++)
+		{
+			if(imgSubtractEroded.at<uchar>(x, y) > 0 &&
+					(y + DEPLACEMENT_COL > 0 && y + DEPLACEMENT_COL <= imgResultColor.cols) &&
+					(x + DEPLACEMENT_ROW > 0 && x + DEPLACEMENT_ROW <= imgResultColor.rows))
+			{
+				for(int i = 0; i < COLOR_NUMBER; i++)
+				{
+					imgResult[i].at<uchar>(x + DEPLACEMENT_ROW, y + DEPLACEMENT_COL) = imgFrontColorSplit[i].at<uchar>(x, y);
+				}
+
+			}
+		}
+	}
+
+	// Restore result image colors
+	Mat channels[COLOR_NUMBER] = {imgResult[INDEX_RED], imgResult[INDEX_GREEN], imgResult[INDEX_BLUE]};
+	merge(channels, COLOR_NUMBER, imgResultColor);
+
+	// Save result
+	imwrite(IMG_RESULT_PATH, imgResultColor);
+
+	// Display initial images and result
+//	namedWindow( "Back", WINDOW_AUTOSIZE );
+//	namedWindow( "Front", WINDOW_AUTOSIZE );
+//	namedWindow( "Back Blur", WINDOW_AUTOSIZE );
+//	namedWindow( "Front Blur", WINDOW_AUTOSIZE );
+//	namedWindow( "Back Binary", WINDOW_AUTOSIZE );
+//	namedWindow( "Front Binary", WINDOW_AUTOSIZE );
+//	namedWindow( "Subtract", WINDOW_AUTOSIZE );
+//	namedWindow( "Subtract Binary", WINDOW_AUTOSIZE );
+//	namedWindow( "Dilated", WINDOW_AUTOSIZE );
+//	namedWindow( "Eroded", WINDOW_AUTOSIZE );
+	namedWindow( "Result Color", WINDOW_AUTOSIZE );
+
+//	imshow( "Back", imgBack );
+//	imshow( "Front", imgFront );
+//	imshow( "Back Blur", imgBackBlur );
+//	imshow( "Front Blur", imgFrontBlur );
+//	imshow( "Front Binary", imgFrontBinary );
+//	imshow( "Back Binary", imgBackBinary );
+//	imshow( "Subtract", imgSubtract );
+//	imshow( "Subtract Binary", imgSubtractBinary);
+//	imshow( "Dilated", imgSubtractDilated);
+//	imshow( "Eroded", imgSubtractEroded);
+	imshow( "Result Color", imgResultColor );
+
+//	for(uchar i = 0; i < COLOR_NUMBER; i++)
+//	{
+//		imshow( "Front Split  " + to_string(i), imgFrontColorSplit[i]);
+//		imshow( "Result " + to_string(i), imgResult[i]);
+//	}
+
+	// Close all windows when a key is pressed
+	waitKey(0);
+	destroyAllWindows();
 
   return EXIT_SUCCESS;
 }
